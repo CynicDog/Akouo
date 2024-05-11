ed4a582ebb878006c4f03f2b6deaea4f6cd43298
ginsenglee <thecynicdog0328@gmail.com>
2024-05-11 23:53:27 +0900

    Chore:

--- src/main/java/io/cynicdog/OAuthAPI.java
+++ src/main/java/io/cynicdog/OAuthAPI.java
@@ -1,5 +1,6 @@
 package io.cynicdog;
 
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.Cookie;
 import io.vertx.core.json.JsonObject;
@@ -11,9 +12,9 @@ import io.vertx.ext.web.client.WebClient;
 import org.jboss.logging.Logger;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 
-import static io.cynicdog.util.OAuthClientUtil.getSpotifyAccessToken;
-import static io.cynicdog.util.OAuthClientUtil.getSpotifyMeInfo;
+import static io.cynicdog.util.SecureStringUtil.encodeBase64;
 import static io.cynicdog.util.SecureStringUtil.generateRandomString;
 import static io.vertx.ext.auth.oauth2.OAuth2FlowType.AUTH_CODE;
 
@@ -81,11 +82,49 @@ public class OAuthAPI {
 
             WebClient client = WebClient.create(ctx.vertx());
 
-            var tokenFuture = getSpotifyAccessToken(client, code, CLIENT_ID, CLIENT_SECRET, redirectUri);
+            var tokenFuture = getSpotifyAccessToken(client, code);
             tokenFuture
                     .thenAccept(result -> handleTokenSuccess(ctx, client, result))
                     .exceptionally(ex -> handleTokenFailure(ctx, ex));
         }
+    }
+
+    private CompletableFuture<JsonObject> getSpotifyAccessToken(WebClient client, String code) {
+
+        CompletableFuture<JsonObject> tokenFuture = new CompletableFuture<>();
+
+        client.postAbs("https://accounts.spotify.com/api/token")
+                .putHeader("Content-Type", "application/x-www-form-urlencoded")
+                .putHeader("Authorization", "Basic " + encodeBase64(CLIENT_ID + ":" + CLIENT_SECRET))
+                .sendForm(MultiMap.caseInsensitiveMultiMap()
+                        .set("code", code)
+                        .set("redirect_uri", redirectUri)
+                        .set("grant_type", AUTH_CODE.getGrantType()), ar -> {
+                    if (ar.succeeded()) {
+                        tokenFuture.complete(ar.result().bodyAsJsonObject());
+                    } else {
+                        tokenFuture.completeExceptionally(ar.cause());
+                    }
+                });
+
+        return tokenFuture;
+    }
+
+    private CompletableFuture<JsonObject> getSpotifyMeInfo(WebClient client, String accessToken) {
+
+        CompletableFuture<JsonObject> userInfoFuture = new CompletableFuture<>();
+
+        client.getAbs("https://api.spotify.com/v1/me")
+                .putHeader("Authorization", "Bearer " + accessToken)
+                .send(ar -> {
+                    if (ar.succeeded()) {
+                        userInfoFuture.complete(ar.result().bodyAsJsonObject());
+                    } else {
+                        userInfoFuture.completeExceptionally(ar.cause());
+                    }
+                });
+
+        return userInfoFuture;
     }
 
     private void handleTokenSuccess(RoutingContext ctx, WebClient client, JsonObject tokenResponse) {
